0401520e7293cf8294bcd999f81f4c62735180aa
Jose-Vela <joseeloyvo@gmail.com>
2023-10-23 21:16:58 -0600

    Diseño de la UI:
    - Comenzamos a diseñar la UI agregando 2 campos de texto (OutlinedTextField) para User y Password, respectivamente y un botón para Login.

--- app/src/main/java/com/example/loginformjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/loginformjetpackcompose/MainActivity.kt
@@ -1,46 +1,83 @@
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+
 package com.example.loginformjetpackcompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.loginformjetpackcompose.ui.theme.LoginFormJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginFormJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            LoginForm()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginForm() {
+    Screan {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
+        ){
+            UserField()
+            PasswordField()
+            LoginButton()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+private fun LoginButton() {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Login")
+    }
+}
+
+@Composable
+private fun PasswordField() {
+    OutlinedTextField(
+        value = "",
+        onValueChange = { },
+        label = { Text(text = "Password") }
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+private fun UserField() {
+    OutlinedTextField(
+        value = "",
+        onValueChange = {},
+        label = { Text(text = "User") }
+    )
+}
+
+@Composable
+fun Screan(content: @Composable () -> Unit) {
     LoginFormJetpackComposeTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+            content = content
+        )
     }
 }
